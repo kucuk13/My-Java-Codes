4c79e52116d157d640d81fb0517103a78b6d1ee3
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2024-01-07 14:49:17 +0300

    edits

--- DollarCostingAverageCalculatorForBist.java
+++ DollarCostingAverageCalculatorForBist.java
@@ -9,17 +9,17 @@ public class DollarCostingAverageCalculatorForBist {
     public static Double TotalAmount = 0d;
 
     public static void main(String[] args) throws Exception {
-        List<Double> priceList = parsePrices(DollarCostingAverageCalculatorInputForBist.prices);
-        List<Double> trUsdPriceList = parsePrices(DollarCostingAverageCalculatorInputForDollar.prices);
+        List<Double> priceList = parsePrices(DollarCostingAverageCalculatorInput.prices);
+        List<Double> trUsdPriceList = parsePrices(DollarCostingAverageCalculatorInputForTrUsdPrices.prices);
         int i = 0;
         for (Double price : priceList){
-            TotalInvest += DollarCostingAverageCalculatorInputForBist.monthlyAmount;
-            TotalAmount += DollarCostingAverageCalculatorInputForBist.monthlyAmount * trUsdPriceList.get(i) / price;
+            TotalInvest += DollarCostingAverageCalculatorInput.monthlyAmount;
+            TotalAmount += DollarCostingAverageCalculatorInput.monthlyAmount * trUsdPriceList.get(i) / price;
             i++;
         }
         System.out.println("You had invested " + TotalInvest);
         System.out.println("You have " + TotalAmount + " stock.");
-        System.out.println("You have " + ((TotalAmount * DollarCostingAverageCalculatorInputForBist.todayPrice) / DollarCostingAverageCalculatorInputForDollar.todayPrice) + " $");
+        System.out.println("You have " + ((TotalAmount * DollarCostingAverageCalculatorInput.todayPrice) / DollarCostingAverageCalculatorInputForTrUsdPrices.todayPrice) + " $");
     }
 
     public static List<Double> parsePrices(String data) {
@@ -28,8 +28,8 @@ public class DollarCostingAverageCalculatorForBist {
 
         for (String line : lines) {
             String[] columns = line.split("\t");
-            if (columns.length > DollarCostingAverageCalculatorInputForBist.columnNo) {
-                String priceString = convertPunctuation(columns[DollarCostingAverageCalculatorInputForBist.columnNo]);
+            if (columns.length > DollarCostingAverageCalculatorInput.columnNo) {
+                String priceString = convertPunctuation(columns[DollarCostingAverageCalculatorInput.columnNo]);
                 try {
                     Double price = Double.parseDouble(priceString);
                     prices.add(price);
